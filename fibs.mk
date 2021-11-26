266086c17462be36446a25b91153c028412ac3aa
Copolycube <kaourintin+github@gmail.com>
2021-11-26 14:40:09 -0500

    Update fibs.mk

--- fibs.mk
+++ fibs.mk
@@ -12,7 +12,8 @@ SHELL != which bash
 tmp := tmp/fib
 
 hosts := tst2 tst4 tst5 tst6 tst8
-host := $$(shuf -n 1 -e $(hosts))
+hosts_avail := $$(while [[ -z `for i in $(hosts); do if ! pgrep -f "ssh $i" -c &>/dev/null ; then echo -n "$i "; fi ; done` ]] ; do echo "no host available"; sleep 2; done )
+host := $$(shuf -n 1 -e $(hosts_avail))
 
 fib := fib () { echo "define fib (n) { if (n <= 2) return 1; return fib(n - 1) + fib(n - 2) }; fib($${1:?})" | bc -ql; }
 
